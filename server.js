4b6bddd4cfd22c13b8db8f0dae404a7e392397c1
JamesJPorter <porterjjames@gmail.com>
2022-10-30 23:21:31 -0700

    added static file to use

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
