461e7dbd4f1869426e2bd1f37daebffbcf85fe02
JamesJPorter <porterjjames@gmail.com>
2022-10-31 00:12:17 -0700

    added some middleware functionality

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static('/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
@@ -16,3 +18,7 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
